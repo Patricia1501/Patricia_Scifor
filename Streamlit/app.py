68577961645e0c627cbdd8d896d8ffa1f53f3074
Patricia Stanley <130581948+Patricia1501@users.noreply.github.com>
2024-06-27 12:45:53 +0530

    Add files via upload

---
+++ Streamlit/app.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+
+
+st.title('Hello Streamlit!')
+
+st.header("Let's see if it works!")
+st.subheader("Yes it does!") 
+st.text("I like Python and Streamlit")
+
+st.image("Image.jpg")
+
+st.button("Click Me!")
+
+st.video("Galaxy.MP4")
